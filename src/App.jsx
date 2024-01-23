6ca4432045ca435cac55cc8fe586a2939f2e4042
Abdulrahman <belloabdulrahman13@gmail.com>
2024-01-23 11:41:50 +0100

    Third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import ImageSlider from './ImageSlide'
 import Twitter from './Twitter'
+import circuit from '/src/assets/circuitslice.png'
 
 
 function App() {
@@ -10,9 +11,12 @@ function App() {
       <div className='h-3/4 mt-8 flex gap-8'>
         <ImageSlider/>
         <div>
-          <button className='border-2 mt-4 border-content p-2 px-3 text-xl float-right rounded-lg lg:mr-7'> <a href="https://babthedev.substack.com/archive" target='_blank'>Check out the Archive</a></button>
+          <div className="flex  mt-4 justify-between">
+          <a href="https://babthedev.substack.com/" target='_blank'><img src={circuit} width='80px' className='rounded-3xl' alt="Circuit Slice Logo" /></a>
+          <button className='border-2 border-content p-2 px-3 text-xl float-right rounded-lg lg:mr-7 hover:bg-gray-100 dark:hover:text-bg dark:hover:border-'> <a href="https://babthedev.substack.com/archive" target='_blank'>Check out the Archive</a></button>
+          </div>
           <p className='mt-28 text-2xl'>The <span className='font-bold text-4xl'>Circuit Slice</span> newsletter</p>
-          <p className='mt-4'>Every week we bring you updates about tech throughout the week.</p>
+          <p className='mt-4 text-lg'>Every week we bring you updates about tech throughout the week.</p>
           <p className='mt-4 text-lg lg:text-2xl'>Join other members of the <span className='font-bold text-2xl lg:text-4xl'>Circuit Network</span> reading this newsletter in their inbox.</p>
           <iframe
         title="My Substack"
